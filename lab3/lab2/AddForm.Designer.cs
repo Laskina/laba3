717599c58f26ee33c25cced64e7ec295b45202e2
Ласкина <34605616+Laskina@users.noreply.github.com>
2018-12-29 00:19:13 +0300

    Add files via upload

---
+++ lab3/lab2/AddForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace lab2
+{
+    partial class AddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_name = new System.Windows.Forms.Label();
+            this.lb_weight = new System.Windows.Forms.Label();
+            this.lb_price = new System.Windows.Forms.Label();
+            this.num_weight = new System.Windows.Forms.NumericUpDown();
+            this.num_price = new System.Windows.Forms.NumericUpDown();
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.cmBox_name = new System.Windows.Forms.ComboBox();
+            this.btn_more = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.num_weight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_price)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_name.Location = new System.Drawing.Point(16, 22);
+            this.lb_name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(96, 20);
+            this.lb_name.TabIndex = 0;
+            this.lb_name.Text = "Название:";
+            // 
+            // lb_weight
+            // 
+            this.lb_weight.AutoSize = true;
+            this.lb_weight.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_weight.Location = new System.Drawing.Point(16, 78);
+            this.lb_weight.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_weight.Name = "lb_weight";
+            this.lb_weight.Size = new System.Drawing.Size(45, 20);
+            this.lb_weight.TabIndex = 1;
+            this.lb_weight.Text = "Вес:";
+            // 
+            // lb_price
+            // 
+            this.lb_price.AutoSize = true;
+            this.lb_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_price.Location = new System.Drawing.Point(16, 133);
+            this.lb_price.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_price.Name = "lb_price";
+            this.lb_price.Size = new System.Drawing.Size(107, 20);
+            this.lb_price.TabIndex = 2;
+            this.lb_price.Text = "Стоимость:";
+            // 
+            // num_weight
+            // 
+            this.num_weight.Location = new System.Drawing.Point(127, 78);
+            this.num_weight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.num_weight.Name = "num_weight";
+            this.num_weight.Size = new System.Drawing.Size(160, 22);
+            this.num_weight.TabIndex = 3;
+            // 
+            // num_price
+            // 
+            this.num_price.Location = new System.Drawing.Point(127, 128);
+            this.num_price.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.num_price.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.num_price.Name = "num_price";
+            this.num_price.Size = new System.Drawing.Size(160, 22);
+            this.num_price.TabIndex = 4;
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btn_OK.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_OK.Location = new System.Drawing.Point(20, 186);
+            this.btn_OK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(172, 47);
+            this.btn_OK.TabIndex = 6;
+            this.btn_OK.Text = "ОК";
+            this.btn_OK.UseVisualStyleBackColor = true;
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Cancel.Location = new System.Drawing.Point(248, 186);
+            this.btn_Cancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(188, 47);
+            this.btn_Cancel.TabIndex = 7;
+            this.btn_Cancel.Text = "Закрыть";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // cmBox_name
+            // 
+            this.cmBox_name.FormattingEnabled = true;
+            this.cmBox_name.Items.AddRange(new object[] {
+            "Книга",
+            "Тетрадь",
+            "Пенал",
+            "Ручка",
+            "Карандаш",
+            "Телефон",
+            "Компьютер",
+            "Зеркало",
+            "Блокнот",
+            "Учебник",
+            "Вода",
+            "Перекус",
+            "Пудра",
+            "Зарядка",
+            "Ключи"});
+            this.cmBox_name.Location = new System.Drawing.Point(127, 22);
+            this.cmBox_name.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmBox_name.Name = "cmBox_name";
+            this.cmBox_name.Size = new System.Drawing.Size(160, 24);
+            this.cmBox_name.TabIndex = 9;
+            // 
+            // btn_more
+            // 
+            this.btn_more.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btn_more.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_more.Location = new System.Drawing.Point(324, 78);
+            this.btn_more.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_more.Name = "btn_more";
+            this.btn_more.Size = new System.Drawing.Size(96, 48);
+            this.btn_more.TabIndex = 8;
+            this.btn_more.Text = "Добавить ещё...";
+            this.btn_more.UseVisualStyleBackColor = true;
+            this.btn_more.Click += new System.EventHandler(this.btn_more_Click);
+            // 
+            // AddForm
+            // 
+            this.AcceptButton = this.btn_OK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Cancel;
+            this.ClientSize = new System.Drawing.Size(449, 240);
+            this.Controls.Add(this.cmBox_name);
+            this.Controls.Add(this.btn_more);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_OK);
+            this.Controls.Add(this.num_price);
+            this.Controls.Add(this.num_weight);
+            this.Controls.Add(this.lb_price);
+            this.Controls.Add(this.lb_weight);
+            this.Controls.Add(this.lb_name);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(467, 287);
+            this.MinimumSize = new System.Drawing.Size(467, 287);
+            this.Name = "AddForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Добавление вещи";
+            ((System.ComponentModel.ISupportInitialize)(this.num_weight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_price)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_name;
+        private System.Windows.Forms.Label lb_weight;
+        private System.Windows.Forms.Label lb_price;
+        private System.Windows.Forms.NumericUpDown num_weight;
+        private System.Windows.Forms.NumericUpDown num_price;
+        private System.Windows.Forms.Button btn_OK;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.ComboBox cmBox_name;
+        private System.Windows.Forms.Button btn_more;
+    }
+}
---
+++ lab3/lab2/MainForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class MainForm : Form
+    {
+        AddForm addForm;
+        Item item_;
+        private List<Item> items;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            item_ = new Item("", 0, 0);
+            items = new List<Item>();
+            addForm = new AddForm(items, LstVw);
+            btn_Task.Enabled = false;
+
+            GrVw.RowCount = 3;
+            GrVw[0, 0].Value = "Макс. стоимость";
+            GrVw[0, 1].Value = "Вес";
+            GrVw[0, 2].Value = "Время";
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (addForm.IsDisposed)
+                addForm = new AddForm(items, LstVw);
+
+            if (!addForm.Visible)
+                addForm.Show();
+
+            if (items != null) 
+               btn_Task.Enabled = true;
+        }
+
+        private void showItems(List<Item> _items, ListView LstVw)
+        {
+            LstVw.Items.Clear();
+            foreach (Item i in _items)
+            {
+                LstVw.Items.Add(new ListViewItem(new string[] { i.name, i.weigth.ToString(),
+                    i.price.ToString() }));
+            }
+        }
+
+        private void btn_Task_Click(object sender, EventArgs e)
+        {
+            int maxW = Convert.ToInt32(num_size.Value);
+            if (maxW == 0)
+            {
+                MessageBox.Show("Максимальный вес рюкзака не может быть равен 0!");
+            }
+            else
+            {
+                Backpack bp = new Backpack(maxW);
+
+                string timeDin, timeGreedy;
+
+                /* Динамическое программирование*/
+                int maxSumDin = bp.dinamicAlg(items, out timeDin);
+                bp.thingsInBackpack(items.Count, items, maxW);
+                List<Item> solveDin = bp.getBestSetDin();
+
+                /* Жадный алгорим */
+                int maxSumGreedy = bp.greedyAlg(items, out timeGreedy);                
+                List<Item> solveGreedy = bp.getBestSetGreedy();
+
+                if (solveDin == null)
+                {
+                    MessageBox.Show("Нет решения!");
+                }
+                else
+                {               
+                    /* Динамическое программирование*/
+                    showItems(solveDin, LstVwDin);
+
+                    GrVw[1, 0].Value = maxSumDin;
+                    GrVw[1, 1].Value = bp.getWeight(solveDin);
+                    GrVw[1, 2].Value = timeDin;
+
+                    /* Жадный алгорим */
+                    showItems(solveGreedy, LstVwGreedy);
+
+                    GrVw[2, 0].Value = maxSumGreedy;
+                    GrVw[2, 1].Value = bp.getWeight(solveGreedy);
+                    GrVw[2, 2].Value = timeGreedy;
+
+                }
+            }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    GrVw[i, j].Value = "";
+
+            LstVw.Items.Clear();
+            LstVwDin.Items.Clear();
+            LstVwGreedy.Items.Clear();
+            items.Clear();
+        }
+    }
+}
